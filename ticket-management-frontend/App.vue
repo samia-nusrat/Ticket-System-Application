44c64a85b065fce003780ea0fea59e9014b53d83
LAPTOP-0OPNKNU8\samia <samian@mun.ca>
2024-09-16 12:51:07 -0230

    Resolve merge conflicts

---
+++ ticket-management-frontend/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="app">
+      <TicketManager />
+    </div>
+  </template>
+  
+  <script>
+  import TicketManager from './components/TicketManager.vue';
+  
+  export default {
+    name: 'App',
+    components: {
+      TicketManager
+    }
+  };
+  </script>
+  
